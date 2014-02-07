3c7ded8cc9d1dd1745a342c865e6f0bae9bd6b99
Renliang Gu (co3125-07) <gurenliang@gmail.com>
2014-02-07 13:02:29 -0600

    _

--- beamharden/testFRI.m
+++ beamharden/testFRI.m
@@ -1,15 +1,15 @@
 
 clear;
-[Imea,args] = genBeamHarden('showImg',false);
+[Imea,args] = genBeamHarden('showImg',false, 'spark',false);
 
-M = 64; % length of the sampled signal;
+M = 2^12; % length of the sampled signal;
 N = 17; % assume roughly there are at most N components;
 L = 32; % the length of the annihalating filter
 % The relationship are:
 %      M/2 + 1 >= L >= N+1
 
 [s, idx] = sort(args.s);
-y = Imea(idx);
+y = Imea(idx).*s;
 
 sameS = (s(1:end-1)-s(2:end))==0;
 s(sameS)=[];
@@ -17,8 +17,10 @@ y(sameS)=[];
 
 sm = linspace(s(1), s(end), M)';
 ym = interp1( s, y, sm,'spline');
+figure; plot(s,y); hold on; plot(sm,ym,'r.');
+figure;
 
-for L = 8:33
+for L = 3:33
     A = [];
     for i=1:L-1
         A = [A ym(L-i:M-i)];
@@ -26,8 +28,8 @@ for L = 8:33
     b = -ym(L:M);
     h = A\b;
     r = roots([1; h]);
-    plot(r,'o');
-    pause(1);
+    plot(r,'o'); title(['L= ' num2str(L)]);
+    pause();
 end
 
 
