55ea508ff81a80df0b8f683a9feaf9ae7da6a5b5
Renliang Gu (isu public) <gurenliang@gmail.com>
2014-02-09 05:25:38 -0600

    _

--- beamharden/runIcip2014.m
+++ beamharden/runIcip2014.m
@@ -5,7 +5,7 @@ function runIcip2014(runList)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %   Author: Renliang Gu (renliang@iastate.edu)
-%   $Revision: 0.2 $ $Date: Sun 09 Feb 2014 05:23:29 AM CST
+%   $Revision: 0.2 $ $Date: Sun 09 Feb 2014 05:25:24 AM CST
 %   v_0.2:      Changed to class oriented for easy configuration
 
 filename = [mfilename '.mat'];
@@ -229,7 +229,7 @@ function [conf, opt] = defaultInit()
     opt.K=2;
     opt.E=17;
     opt.useSparse=0;
-    opt.showImg=1;
+    opt.showImg=0;
     opt.visible=1;
     opt.skipAlpha=0;
     opt.maxIeSteps = 1;
