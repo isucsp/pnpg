ecf8566550f074b999e2c7aeba69b09e0b82b984
Renliang Gu (co3125-07) <gurenliang@gmail.com>
2014-02-17 18:51:08 -0600

    _

---
+++ beamharden/subfunctions/FBPFunc7.m
@@ -0,0 +1,49 @@
+function x = FBPFunc7(y,theta_full,theta_idx,Ts,maskIdx)
+% FBPFunc7      Filtered back projection reconstruction
+%   This routine is implemented by the external C language. The C code 
+%   implement the projection and back projection by the discrete methods.
+%   x: wavelet coefficients vector
+%   y: the input projection data in frequency domain
+%   f: coefficients of the filter applying on y
+%   st: 
+%   h: wavelet scaling filter
+%   L: level of wavelet decomposition
+%   m: size of image
+%   out = beamharden(***)
+%   Phi         The projection matrix implementation function handle
+%   Phit        Transpose of Phi
+%   Psi         Inverse wavelet transform matrix from wavelet coefficients
+%               to image.
+%   Psit        Transpose of Psi
+%   y           Log scale of Beamhardening measurement y=-log(I^{mea}/I_0)
+%   xInit       Initial value for the algorithm
+%   opt         Structure for the configuration of this algorithm (refer to
+%               the code for detail)
+%
+%   Reference:
+%   Author: Renliang Gu (renliang@iastate.edu)
+%   $Revision: 0.1 $ $Date: Sun Sep 16 19:31:58 CDT 2012
+
+n=length(theta_idx(:));
+m=length(y(:))/n;
+y=reshape(y,m,n);
+y=[zeros(m/2,n); y; zeros(m/2,n)];
+[Num_pixel,Num_proj]=size(y);
+
+theta=theta_full(theta_idx);
+
+f=designFilter('renliang1',m*2,Ts);
+
+y=fftshift(fft(fftshift(y,1)),1)*Ts;
+y=y.*repmat(f(:),1,Num_proj);
+
+y=fftshift(ifft(fftshift(y,1)),1)/Ts;
+y=real(y);
+y=y(m/2+1:m/2+m,:);
+[Num_pixel,Num_proj]=size(y);
+conf.bw=1; conf.nc=Num_pixel; conf.nr=Num_pixel; conf.prjWidth=Num_pixel;
+conf.theta=theta*180/pi;
+out=mParPrj(y,maskIdx-1,conf,'backward');
+x=zeros(m,m);
+x(maskIdx)=out(1:length(maskIdx));
+
