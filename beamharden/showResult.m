3e4aac388c22126a3639abde92d790673968aa5f
Renliang Gu (co3125-07) <gurenliang@gmail.com>
2014-03-29 09:05:04 -0500

    _

--- beamharden/showResult.m
+++ beamharden/showResult.m
@@ -1,15 +1,29 @@
-function res = plotVector(out,field,plotf)
+function summary = plotVector(out,field,plotf)
     color  = {'r' ,'g' ,'b' ,'k' ,'c' ,'y' ,'m'};
     lines  = {'-' ,'--',':' ,'-.'};
     style2 = {'b-*','r-*','b:o','r:o','b--s','r--s'};
 
+    maxlen=1;
     figure;
-    for i=1:length(out)
-        res{i}=getfield(out{i},field);
-        plotf(res{i},'color',color{mod(i,length(color))+1},...
-            'linestyle',lines{mod(i,length(lines))+1});
-        hold on;
+    for i=1:length(out(:))
+        if(~isempty(out{i}))
+            res{i}=getfield(out{i},field);
+            plotf(res{i},'color',color{mod(i-1,length(color))+1},...
+                'linestyle',lines{mod(i-1,length(lines))+1});
+            hold on;
+            temp=res{i};
+
+            if(length(temp(:))>maxlen) maxlen=length(temp(:)); end
+        else
+            plotf(0); hold on;
+        end
         str{i}=num2str(i);
     end
     legend(str);
+    summary=zeros(maxlen, length(out(:)));
+    for i=1:length(out(:))
+        temp=res{i};
+        summary(1:length(temp),i)=temp(:);
+    end
+    
 end
