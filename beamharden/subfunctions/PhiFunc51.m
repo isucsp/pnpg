d599a95dc537a07355ec9255ded0bd11f985b1ff
Renliang Gu (co3125-07) <gurenliang@gmail.com>
2015-08-13 16:32:11 -0500

    fixed the scaling problem of FBP

--- beamharden/subfunctions/PhiFunc51.m
+++ beamharden/subfunctions/PhiFunc51.m
@@ -8,13 +8,12 @@ function y = PhiFunc51(x,f,st,m,Ts,mask)
 % m: size of image
 %   Reference:
 %   Author: Renliang Gu (renliang@iastate.edu)
-%   $Revision: 0.1 $ $Date: Thu 07 May 2015 10:27:04 AM CDT
+%   $Revision: 0.1 $ $Date: Thu 13 Aug 2015 12:59:32 PM CDT
 
 if(nargin>5)
     u=mask.b(x);
-else
-    u=reshape(x,m,m);
 end
+u=reshape(x,m,m);
 y=nufft(u,st)*Ts^2;
 y=reshape(y,st.Num_pixel,st.Num_proj);
 y=fftshift(y,1);
