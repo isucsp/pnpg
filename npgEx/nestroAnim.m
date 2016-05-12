19cdfef8de610e403365f663bbe572fd9ca6de37
Renliang Gu <gurenliang@gmail.com>
2016-05-12 13:05:59 -0500

    _

--- npgEx/nestroAnim.m
+++ npgEx/nestroAnim.m
@@ -2,14 +2,14 @@
 close all;
 clear all;
 
-opt.maxItr=5e4; opt.thresh=1e-116; opt.debugLevel=1;
-A=[1,1.21;0.5,3];
-y=poisson(A*[1;1]*100)/100;
-y=A*[0.1;1];
+A=[1,0.21;0.5,3];
+x0=[0.1;0.1];
+y=poisson(A*x0*100)/100;
+y=A*x0;
 f=@(a,b) (A(1,1)*a+A(1,2)*b-y(1)+A(2,1)*a+A(2,2)*b-y(2))...
     +y(1)*log(y(1)./(A(1,1)*a+A(1,2)*b))...
     +y(2)*log(y(2)./(A(2,1)*a+A(2,2)*b));
-[x1,x2]=meshgrid(-1:0.01:3, -1:0.01:3);
+[x1,x2]=meshgrid(linspace(-1,3*x0(1),1000), linspace(-1,3*x0(2),1000));
 fx=f(x1,x2);
 Phi=@(x) A*x;
 Phit=@(x) A.'*x;
@@ -24,6 +24,8 @@ xInit=[5.5,0.5];
 xInit=[4,5];
 xInit=[3,0];
 xInit=[1,1]*1e-5;
+
+opt.maxItr=5e4; opt.thresh=1e-6; opt.debugLevel=1;
 opt.u=1e-19;
 opt.L = max(svd(A))^2;
 opt.continuation=false; opt.alphaStep='pnpg'; opt.saveXtrace=true;
