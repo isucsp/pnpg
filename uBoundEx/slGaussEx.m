94e577e09152b376aca5aa35043a0d07547bd038
Renliang Gu (isu public) <gurenliang@gmail.com>
2016-09-16 02:24:05 -0500

    _

--- uBoundEx/slGaussEx.m
+++ uBoundEx/slGaussEx.m
@@ -32,23 +32,22 @@ switch lower(op)
                 PsiM=Utils.getMat(Psi,length(Psit(OPT.trueAlpha)));
                 Phity=Phit(y);
 
-                cvx_begin
-                    variable a(p)
-                    minimize( norm( PsiM'*(Phity+a), inf) )
-                    subject to
-                        a>=0
-                cvx_end
+                %cvx_begin
+                %    variable a(p)
+                %    minimize( norm( PsiM'*(Phity+a), inf) )
+                %    subject to
+                %        a>=0
+                %cvx_end
 
-                u_1(i)=cvx_optval;
-                
-                Pncx=@(x) min(x,0);
-                u_2(i)=uBound(Psi,Psit,Pncx,zeros(p,1),-Phit(y));
+                %u_1(i)=cvx_optval;
+                %
+                %Pncx=@(x) min(x,0);
+                %u_2(i)=uBound(Psi,Psit,Pncx,zeros(p,1),-Phit(y));
 
                 opt=OPT;
                 initSig=zeros(size(opt.trueAlpha)); ur=u_1(i)*100; ul=0;
                 ur_rmse=0; ul_rmse=0;
 
-                keyboard
                 while(ur-ul>1e-5)
                     fprintf('%10g <-> %10g\n',ul,ur);
                     fprintf('%10g <-> %10g\n',ul_rmse,ur_rmse);
@@ -61,10 +60,41 @@ switch lower(op)
                     else
                         ul=opt.u; ul_rmse=rmse;
                     end
-                    pause(2);
                 end
                 u_rmse(i)=ur_rmse;
                 u_3(i)=ur;
+
+                cvx_begin
+                    variable a(p)
+                    minimize( norm( PsiM'*(Phity+a), inf) )
+                    subject to
+                        a>=0
+                cvx_end
+
+                u_4(i)=norm( PsiM'*(Phity), inf);
+                
+                Pncx=@(x) x*0;
+                u_5(i)=uBound(Psi,Psit,Pncx,zeros(p,1),-Phit(y));
+
+                opt=OPT;
+                initSig=zeros(size(opt.trueAlpha)); ur=u_1(i)*100; ul=0;
+                ur_rmse=0; ul_rmse=0;
+
+                while(ur-ul>1e-5)
+                    fprintf('%10g <-> %10g\n',ul,ur);
+                    fprintf('%10g <-> %10g\n',ul_rmse,ur_rmse);
+                    opt.u=(ur+ul)/2; opt.thresh=1e-9;
+                    fprintf('u=%g\n',opt.u);
+                    out=Wrapper.NPGs(Phi,Phit,Psi,Psit,y,initSig,opt);
+                    rmse=norm(out.alpha)
+                    if(rmse<=eps)
+                        ur=opt.u; ur_rmse=rmse;
+                    else
+                        ul=opt.u; ul_rmse=rmse;
+                    end
+                end
+                u_rmse(i)=ur_rmse;
+                u_6(i)=ur;
 
                 mysave;
             end;
