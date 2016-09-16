ea8ef7b6d94ae358c5904e52ca32132f7d79e41a
Renliang Gu (isu public) <gurenliang@gmail.com>
2016-09-16 13:33:27 -0500

    _

--- uBoundEx/tv_Bound.m
+++ uBoundEx/tv_Bound.m
@@ -26,58 +26,42 @@ switch lower(op)
         K=1;
         count = [1e4 1e5 1e6 1e7 1e8 1e9];
         for k=1:K
-            for i=4:length(count)
+            for i=1:length(count)
                 fprintf('%s, i=%d, j=%d, k=%d\n','PET Example',i,1,k);
                 [y,Phi,Phit,Psi,Psit,fbpfunc,opt]=loadPET(count(i),opt);
 
                 [x0s,g]=Utils.poissonModelConstEst(Phi,Phit,y,opt.bb,1e-16);
                 g=reshape(g,sqrt(length(g(:))),[]);
 
-                %u_maxANI(i,k)=TV.upperBoundU(maskFunc(g,opt.mask));
-                %u_maxISO(i,k)=sqrt(2)*u_maxANI(i,k);
-                %u_maxANI_dual(i,k)=TV.upperBoundU_dual(maskFunc(g,opt.mask));
-                %u_maxISO_dual(i,k)=sqrt(2)*u_maxANI_dual(i,k);
-                %u_maxANI_admm(i,k)=TV.upperBoundU_admm(maskFunc(g,opt.mask));
-                %u_maxANI_adm2(i,k)=TV.upperBoundU_admm2(maskFunc(g,opt.mask));
-                %keyboard
-                u_admm(i,k)=TV.upperBoundU_admm3(g,x0s*ones(size(g)));
+                u_1(i)=TV.upperBoundU_admm3(g,x0s*ones(size(g)));
 
-                continue;
                 initSig=ones(size(opt.trueAlpha))*x0s;
 
-                rmse=0; opt.u=u_maxANI(i,k); opt.proximal='tvl1';
-                while(rmse==0)
-                    opt.u = 0.9*opt.u;
-                    fprintf('u=%g\n',opt.u);
-                    out=Wrapper.NPG(Phi,Phit,[],[],y,initSig,opt);
-                    rmse=norm(out.alpha-initSig);
-                end
-                opt.u=opt.u/0.9; rmse=0;
-                while(rmse==0)
-                    opt.u = 0.99*opt.u;
-                    fprintf('u=%g\n',opt.u);
-                    out=Wrapper.NPG(Phi,Phit,[],[],y,initSig,opt);
-                    rmse=norm(out.alpha-initSig);
-                end
-                u_trueANI(i,k)=opt.u/0.99;
+                ur=u_1(i)*100; ul=0; ur_rmse=0; ul_rmse=0;
+                opt.proximal='tvl1';
 
-                rmse=0; opt.u=u_maxISO(i,k); opt.proximal='tviso';
-                while(rmse==0)
-                    opt.u = 0.9*opt.u;
+                while(ur-ul>1e-5)
+                    fprintf('%10g <-> %10g\n',ul,ur);
+                    fprintf('%10g <-> %10g\n',ul_rmse,ur_rmse);
+                    opt.u=(ur+ul)/2; opt.thresh=1e-9;
                     fprintf('u=%g\n',opt.u);
-                    out=Wrapper.NPG(Phi,Phit,[],[],y,initSig,opt);
-                    rmse=norm(out.alpha-initSig);
+                    out=Wrapper.PNPG(Phi,Phit,[],[],y,initSig,opt);
+                    rmse=norm(out.alpha-initSig)
+                    if(rmse<=eps)
+                        ur=opt.u; ur_rmse=rmse;
+                    else
+                        ul=opt.u; ul_rmse=rmse;
+                    end
                 end
-                opt.u=opt.u/0.9; rmse=0;
-                while(rmse==0)
-                    opt.u = 0.99*opt.u;
-                    fprintf('u=%g\n',opt.u);
-                    out=Wrapper.NPG(Phi,Phit,[],[],y,initSig,opt);
-                    rmse=norm(out.alpha-initSig);
-                end
-                u_trueISO(i,k)=opt.u/0.99;
+
+                u_2(i)=ur;
+                u_rmse(i)=ur_rmse;
+
+                mysave;
             end
         end
+
+    case 'plot'
 
         figure;
         loglog(count,u_maxANI,'b^-'); hold on;
@@ -94,6 +78,5 @@ switch lower(op)
 
         save(filename);
 end
-
 end
 
