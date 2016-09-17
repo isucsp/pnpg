d86c79a807af105d8c3a8fae884ec1a2d18d8c92
Renliang Gu (isu public) <gurenliang@gmail.com>
2016-09-16 23:33:53 -0500

    _

--- uBoundEx/tv_Bound.m
+++ uBoundEx/tv_Bound.m
@@ -33,7 +33,7 @@ switch lower(op)
                 [x0s,g]=Utils.poissonModelConstEst(Phi,Phit,y,opt.bb,1e-16);
                 g=reshape(g,sqrt(length(g(:))),[]);
 
-                u_1(i)=TV.upperBoundU_admm3(g,x0s*ones(size(g)));
+                %u_1(i)=TV.upperBoundU_admm3(g,x0s*ones(size(g)));
 
                 initSig=ones(size(opt.trueAlpha))*x0s;
 
@@ -56,6 +56,24 @@ switch lower(op)
 
                 u_2(i)=ur;
                 u_rmse(i)=ur_rmse;
+
+                ur=u_1(i)*100; ul=0; ur_rmse=0; ul_rmse=0;
+                opt.proximal='tviso';
+                while(ur-ul>1e-5)
+                    fprintf('%10g <-> %10g\n',ul,ur);
+                    fprintf('%10g <-> %10g\n',ul_rmse,ur_rmse);
+                    opt.u=(ur+ul)/2; opt.thresh=1e-9;
+                    fprintf('u=%g\n',opt.u);
+                    out=Wrapper.PNPG(Phi,Phit,[],[],y,initSig,opt);
+                    rmse=norm(out.alpha-initSig)
+                    if(rmse<=eps)
+                        ur=opt.u; ur_rmse=rmse;
+                    else
+                        ul=opt.u; ul_rmse=rmse;
+                    end
+                end
+
+                u_3(i)=ur;
 
                 mysave;
             end
