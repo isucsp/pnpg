e39f78cae64f7543ca3d79c0e3d37449cb016929
Renliang Gu (X201) <gurenliang@gmail.com>
2016-12-12 00:31:34 -0800

    _

--- utils/realInnerProd.m
+++ utils/realInnerProd.m
@@ -1,4 +1,8 @@
 function out = realInnerProd(a,b)
-    out = real(a).*real(b)+imag(a).*imag(b);
+    if(isreal(a) && isreal(b))
+        out = a.*b;
+    else
+        out = real(a).*real(b)+imag(a).*imag(b);
+    end
     out = sum(out(:));
 end
