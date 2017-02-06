7a26226c7f28d5fabc6d00cf98f0423f6236f70c
Renliang Gu (X201) <gurenliang@gmail.com>
2017-02-06 00:45:17 -0800

    _

--- uBoundEx/slGaussEx.m
+++ uBoundEx/slGaussEx.m
@@ -38,51 +38,51 @@ switch lower(op)
 
         % the following are under sparsity regularization +
         % nonnegativity constraints
-%       cvx_begin
-%       variable a(p)
-%       minimize( norm( PsiM'*(Phity+a), inf) )
-%       subject to
-%       a>=0
-%       cvx_end
-%       u_1(i)=cvx_optval;
+        cvx_begin
+        variable a(p)
+        minimize( norm( PsiM'*(Phity+a), inf) )
+        subject to
+        a>=0
+        cvx_end
+        u_1(i)=cvx_optval;
 
-%       Pncx=@(x) min(x,0);
-%       u_2(i)=uBound(Psi,Psit,'wav',Pncx,zeros(p,1),-Phity);
+        Pncx=@(x) min(x,0);
+        u_2(i)=uBound(Psi,Psit,'wav',Pncx,zeros(p,1),-Phity);
 
-%       PROXOPT.Lip=@(u)u^2; PROXOPT.initStep='fixed';
-%       PROXOPT.adaptiveStep=false; PROXOPT.backtracking=false;
-%       PROXOPT.debugLevel=2; PROXOPT.verbose=1e3;
-%       proximal=sparseProximal(Psi,Psit,C.prox,'pnpg',PROXOPT);
-%       cond=@(x) norm(x)/length(x);
-%       %beta=1/OPT.L;
-%       beta=1;  % a step size is not needed sine x*=0
-%       func=@(u) proximal.prox(beta*Phity,u*beta,1e-10,1e5,[]);
-%       %opt=OPT; opt.maxPossibleInnerItr=1e4; opt.trueX=opt.trueX*0;
-%       %func=@(u) pg(NLL,proximal,x0*0,setfield(opt,'u',u));
-%       u_3(i)=bisection(func,cond,u_1(i)/2,u_1(i)*2,1e-6);
+        PROXOPT.Lip=@(u)u^2; PROXOPT.initStep='fixed';
+        PROXOPT.adaptiveStep=false; PROXOPT.backtracking=false;
+        PROXOPT.debugLevel=2; PROXOPT.verbose=1e3;
+        proximal=sparseProximal(Psi,Psit,C.prox,'pnpg',PROXOPT);
+        cond=@(x) norm(x)/length(x);
+        %beta=1/OPT.L;
+        beta=1;  % a step size is not needed sine x*=0
+        func=@(u) proximal.prox(beta*Phity,u*beta,1e-10,1e5,[]);
+        %opt=OPT; opt.maxPossibleInnerItr=1e4; opt.trueX=opt.trueX*0;
+        %func=@(u) pg(NLL,proximal,x0*0,setfield(opt,'u',u));
+        u_3(i)=bisection(func,cond,u_1(i)/2,u_1(i)*2,1e-6);
 
-%       % the following are under sparsity regularization only
-%       u_4(i)=norm( PsiM'*(Phity), inf);
-%       fprintf('u_4=%20.10g\n',u_4(i));
+        % the following are under sparsity regularization only
+        u_4(i)=norm( PsiM'*(Phity), inf);
+        fprintf('u_4=%20.10g\n',u_4(i));
 
-%       Pncx=@(x) x*0;
-%       u_5(i)=uBound(Psi,Psit,'wav',Pncx,zeros(p,1),-Phity);
+        Pncx=@(x) x*0;
+        u_5(i)=uBound(Psi,Psit,'wav',Pncx,zeros(p,1),-Phity);
 
-%       proximal.exact=true;
-%       proximal.val=@(x) norm(Psit(x),1);
-%       proximal.prox=@(x,u) Psi(Utils.softThresh(Psit(x),u));
-%       cond=@(x) norm(x)/length(x);
-%       func=@(u) Psi(Utils.softThresh(Psit(Phity),u));
-%       %opt=OPT; opt.maxPossibleInnerItr=1e4; opt.trueX=opt.trueX*0;
-%       %func=@(u) pg(NLL,proximal,opt.trueX*0,setfield(opt,'u',u));
-%       u_6(i)=bisection(func,cond,u_4(i)/2,u_4(i)*2);
+        proximal.exact=true;
+        proximal.val=@(x) norm(Psit(x),1);
+        proximal.prox=@(x,u) Psi(Utils.softThresh(Psit(x),u));
+        cond=@(x) norm(x)/length(x);
+        func=@(u) Psi(Utils.softThresh(Psit(Phity),u));
+        %opt=OPT; opt.maxPossibleInnerItr=1e4; opt.trueX=opt.trueX*0;
+        %func=@(u) pg(NLL,proximal,opt.trueX*0,setfield(opt,'u',u));
+        u_6(i)=bisection(func,cond,u_4(i)/2,u_4(i)*2);
 
-%       % following is the 1d TV regularization
-%       x0=ones(p,1)*sum(Phity)/sqrNorm(Phi(ones(p,1)));
-%       x0=max(x0,0);  % x0 has to be nonnegative
-%       g=Phit(Phi(x0)-yy);
-%       u_7(i)=norm(cumsum(g),inf);
-%       fprintf('u_7=%20.10g\n',u_7(i));
+        % following is the 1d TV regularization
+        x0=ones(p,1)*sum(Phity)/sqrNorm(Phi(ones(p,1)));
+        x0=max(x0,0);  % x0 has to be nonnegative
+        g=Phit(Phi(x0)-yy);
+        u_7(i)=norm(cumsum(g),inf);
+        fprintf('u_7=%20.10g\n',u_7(i));
 
         if(x0(1)>0)
           Pncx=@(x) x*0;
@@ -97,13 +97,11 @@ switch lower(op)
         cond=@(x) relativeDif(x,mean(x));
         beta=1/OPT.L;
         func=@(u) iso.prox(x0-beta*g,u*beta,1e-11,1e5,[]);
-        opt=OPT; opt.debugLevel=2; opt.maxInnerItr=1e4;
-        opt.errorType=-1; opt.computError=@(x) relativeDif(x,mean(x));
-        func=@(u) pnpg(NLL,proximal,x0,setfield(opt,'u',u));
-        u_9(i)=bisection(func,cond,u_7(i)/2,u_7(i)*1.2, 10^-6);
+        %opt=OPT; opt.debugLevel=2; opt.maxInnerItr=1e4;
+        %opt.errorType=-1; opt.computError=@(x) relativeDif(x,mean(x));
+        %func=@(u) pnpg(NLL,proximal,x0,setfield(opt,'u',u));
+        u_9(i)=bisection(func,cond,u_7(i)/2,u_7(i)*1.2, 10^-8);
 
-        keyboard
- 
         mysave;
         keyboard
       end;
