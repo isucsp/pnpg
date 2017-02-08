830dc380b0fda21663c4f28f4ac84e3164bc3aec
Renliang Gu (X201) <gurenliang@gmail.com>
2017-02-07 23:11:38 -0800

    _

--- uBoundEx/slGaussEx.m
+++ uBoundEx/slGaussEx.m
@@ -115,18 +115,23 @@ switch lower(op)
     m = [ 200, 250, 300, 350, 400, 500, 600, 700, 800]; % should go from 200
     snr = [1e3 100 10 1 0.1 0.01 1e-3];
 
-    forSave=[m; u_1; u_2; u_3; u_4; u_5; u_6]';
+    forSave=[snr; u_1; u_2; u_3; u_4; u_5; u_6; u_7; u_8; u_9]';
 
     figure;
-    plot(m,u_1,'b^-'); hold on;
-    plot(m,u_2,'gh-'); hold on;
-    plot(m,u_3,'bs--');
-    plot(m,u_4,'r*-');
-    plot(m,u_5,'gp-');
-    plot(m,u_6,'ro--');
+    semilogx(snr,u_1,'b^-'); hold on;
+    plot(snr,u_2,'gh-'); hold on;
+    plot(snr,u_3,'bs--');
+    plot(snr,u_4,'r*-');
+    plot(snr,u_5,'gp-');
+    plot(snr,u_6,'ro--');
+    plot(snr,u_7,'r*-');
+    plot(snr,u_8,'gp-');
+    plot(snr,u_9,'ro--');
 
-    rowLabels={'$N$','theoretical','empirical','theoretical','empirical'};
-    matrix2latex(forSave(:,[1 2 4 5 7]), 'slBound.tex', 'columnLabels', rowLabels,...
+    rowLabels={'$N$','theoretical','empirical',...
+        'theoretical','empirical','theoretical','empirical'};
+
+    matrix2latex(forSave(:,[1 2 4 5 7 8 10]), 'slBound.tex', 'columnLabels', rowLabels,...
       'alignment', 'r', 'format', '%-6.2f', 'size', 'small');
     save('slBound.data','forSave','-ascii');
 end
